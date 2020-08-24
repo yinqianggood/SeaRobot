04a498a2039ed6ec3309cdfa3566e95893080b38
尤如梦龙 <whkxa_618_ybz@163.com>
2020-08-24 16:58:16 +0800

    lcq:add some pages

---
+++ Assets/Scripts/UIScript/UITMSMainControl1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITMSMainControl1 : UIPage
+{
+    public UITMSMainControl1() : base(UIType.Normal, UIMode.HideOther, UICollider.None)
+    {
+        uiPath = "UIPrefab/UITMSMainControl1";
+    }
+
+    public override void Awake(GameObject go)
+    {
+
+    }
+    public override void Active()
+    {
+        base.Active();
+        MsgMng.Instance.Send(MessageName.MSG_CHANGE_TITTLE, new MessageData("TMS Main Control1"));
+    }
+}
--- Assets/Scripts/UIScript/UITMSMenu.cs
+++ Assets/Scripts/UIScript/UITMSMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UITMSMenu : UIPage
 {
@@ -12,7 +13,10 @@ public class UITMSMenu : UIPage
 
     public override void Awake(GameObject go)
     {
-
+        this.transform.Find("btn_TMS Main Control1").GetComponent<Button>().onClick.AddListener(() =>
+        {
+            UIPage.ShowPage<UITMSMainControl1>();
+        });
     }
     public override void Active()
     {
