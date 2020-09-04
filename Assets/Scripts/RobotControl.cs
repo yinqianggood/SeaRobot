e63c4658899433f5cf571cd25f647183cf5f0018
yinqianggood <yinqianggood@qq.com>
2020-09-04 16:28:53 +0800

    【yinqiang】 thruster move control

--- Assets/Scripts/RobotControl.cs
+++ Assets/Scripts/RobotControl.cs
@@ -14,7 +14,9 @@ public class RobotControl : MonoBehaviour
     public GameObject armFinger1;
     public GameObject armFinger2;
 
-    public List<Transform> propellers;
+    public List<Transform> H_Propellers = new List<Transform>();
+    public List<Transform> V_Propellers = new List<Transform>();
+    private float mPropSpeed = 500;
 
     public enum DIR
     { 
@@ -27,6 +29,14 @@ public class RobotControl : MonoBehaviour
        TurnL=7,
        TurnR=8
     }
+
+    public enum PROPDIR 
+    {
+      Horizontal=1,
+      Vertical=2,
+      Both=3
+
+    }
     private Rigidbody _rb ;
     private void Awake()
     {
@@ -35,7 +45,7 @@ public class RobotControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        ThrusterControl(300);
+       
     }
 
     // Update is called once per frame
@@ -46,35 +56,43 @@ public class RobotControl : MonoBehaviour
 
     public void MoveROV(int direct,float speed=1,Boolean isPress=true)
     {
-      
+       
         DIR dir = (DIR)direct;
         switch (dir)
         {
             case DIR.Foward:
                 ROV.transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+                ThrusterControl(mPropSpeed, PROPDIR.Horizontal);
                 break;
             case DIR.Back:
                 ROV.transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+                ThrusterControl(-mPropSpeed, PROPDIR.Horizontal);
                 break;
             case DIR.Left:
                 ROV.transform.Translate(new Vector3(0,0,-speed * Time.deltaTime));
+                ThrusterControl(mPropSpeed, PROPDIR.Horizontal);
                 break;
             case DIR.Right:
                 ROV.transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+                ThrusterControl(-mPropSpeed, PROPDIR.Horizontal);
                 break;
             case DIR.Up:
                 ROV.transform.Translate(new Vector3(0, speed * Time.deltaTime,0));
+                ThrusterControl(mPropSpeed, PROPDIR.Vertical);
                 break;
             case DIR.Down:
                 // _rb.velocity = transform.forward* Time.deltaTime * 2;
                 //ROV.transform.position -= transform.up * Time.deltaTime * speed;
                 ROV.transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+                ThrusterControl(-mPropSpeed, PROPDIR.Vertical);
                 break;
             case DIR.TurnL:
                 ROV.transform.Rotate(new Vector3(0, -speed * 5 * Time.deltaTime, 0));
+                ThrusterControl(-mPropSpeed, PROPDIR.Horizontal);
                 break;
             case DIR.TurnR:
                 ROV.transform.Rotate(new Vector3(0, speed* 5 * Time.deltaTime, 0));
+                ThrusterControl(mPropSpeed, PROPDIR.Horizontal);
                 break;
             default:
                 break;
@@ -106,12 +124,23 @@ public class RobotControl : MonoBehaviour
         }
     }
 
-    private void ThrusterControl(float speed)
+    private void ThrusterControl(float speed,PROPDIR pd)
     {
-        //for (int i = 0; i < propellers.Count; i++)
-        //{
-        //    propellers[i].transform.Rotate(new Vector3(0, speed * Time.deltaTime, 0));
-           
-        //}
+        if(pd!=PROPDIR.Vertical)
+        {
+            for (int i = 0; i < H_Propellers.Count; i++)
+            {
+                H_Propellers[i].transform.Rotate(new Vector3(0, speed * Time.deltaTime, 0));
+            }
+        }
+        
+        if(pd!=PROPDIR.Horizontal)
+        {
+            for (int i = 0; i < V_Propellers.Count; i++)
+            {
+                V_Propellers[i].transform.Rotate(new Vector3(0, speed * Time.deltaTime, 0));
+            }
+        }
+       
     }
 }
