3c43ebbb12c46475d526228c3d55197b4c472e45
yinqianggood <yinqianggood@qq.com>
2020-11-03 10:48:17 +0800

    [yinqiang] oneScreen multidispaly

--- Assets/Scripts/GameManager/GameMain.cs
+++ Assets/Scripts/GameManager/GameMain.cs
@@ -6,16 +6,24 @@ using UnityEngine.UI;
 public class GameMain : MonoBehaviour {
 
 	// Use this for initialization
+	public Light directLight;
 	void Start () {
        
         UIPage.ShowPage<UITopBar>();
 
-		// var root = UIRoot.Instance;
-    }
+        // var root = UIRoot.Instance;
+        for (int i = 0; i < Display.displays.Length; i++)
+        {
+			Display.displays[i].Activate();
+        }
+		
+
+
+	}
 	
 	// Update is called once per frame
 	void Update () {
-	    
+		directLight.intensity = ControlData.Instance.ALLROVLAMP_isOn;
 	}
 
 }
